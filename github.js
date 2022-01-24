3cd083ac45663a3fc6e57186d2676aa457cae002
Jacob Cho <jacobcho@live.com>
2022-01-24 14:02:54 +0000

    github app updated

---
+++ github.js
@@ -0,0 +1,10 @@
+class Github {
+  constructor(){
+    this.client_id = '4cae939f309e2b600f6a';
+    this.client_secret = '5f65444a178ffec1a5a88abde21196d98f816ffd';
+  }
+
+  async getUser(user){
+    const profileResponse
+  }
+}
