351f9e5f1c13162c3653a1a4c5a996763a5f1ff1
Jacob Cho <jacobcho@live.com>
2022-02-08 11:33:53 +0000

    repo fetch

--- github.js
+++ github.js
@@ -4,11 +4,16 @@ class Github {
   constructor(){
     this.client_id = '4cae939f309e2b600f6a';
     this.client_secret = '5f65444a178ffec1a5a88abde21196d98f816ffd';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user){
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=$${this.client_secret}`)
   
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?client_id=${this.client_id}&client_secret=$${this.client_secret}`)
+  
+    
     const profileData = await profileResponse.json();
 
     return {
